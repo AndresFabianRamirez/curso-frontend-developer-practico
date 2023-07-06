98134c28dd941717c8ec7331678a55238e83c12e
andres <andrecitoramirez11@gmail.com>
2023-07-06 16:21:17 -0500

    guardado  hasta cambios de la clase 12

--- main.js
+++ main.js
@@ -1,13 +1,108 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuIconBur = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+const productDetail = document.querySelector('.product-detail')
+const carMenu = document.querySelector('.navbar-shopping-cart');
+const cardsContainer =document.querySelector('.cards-container');
+
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
 function toggleDesktopMenu(){
+    if(productDetail.classList.contains('inactive') != true){
+        productDetail.classList.add('inactive');
+     }
     desktopMenu.classList.toggle('inactive');
 }
 menuIconBur.addEventListener('click',toggleMobileMenu);
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+    if(productDetail.classList.contains('inactive') != true){
+        productDetail.classList.add('inactive');
+     }
+     mobileMenu.classList.toggle('inactive');
 }
+carMenu.addEventListener('click',toggleCarMenu);
+function toggleCarMenu(){
+    if(desktopMenu.classList.contains('inactive') != true){
+        desktopMenu.classList.add('inactive');
+     }
+    if(mobileMenu.classList.contains('inactive') != true){
+       mobileMenu.classList.add('inactive');
+    }
+    
+    productDetail.classList.toggle('inactive');
+    
+}
+
+/* <!-- <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div> --> */
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+} 
+) 
+productList.push({
+    name: 'Patineta',
+    price: 70,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+} 
+) 
+productList.push({
+    name: 'Raqueta profesiona',
+    price: 80,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+} 
+) 
+function productos(arr){
+    for (product of arr){
+        const productCard =document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg =document.createElement('img');
+        productImg.setAttribute('src',product.image);
+    
+        const productDiv = document.createElement('div');
+        productDiv.classList.add('product-info');
+    
+        const productInfoDiv =document.createElement('div');
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$'+ product.price;
+    
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productName);
+        productInfoDiv.appendChild(productPrice);
+    
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productDiv.appendChild(productInfoDiv);
+        productDiv.appendChild(productInfoFigure);
+        
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productDiv);
+    
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+productos(productList);
